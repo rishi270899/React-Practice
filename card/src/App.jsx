d640dc85054a5b5e05ffffc72b0bbafdc558fada
Rishi Goyal <rishig516@gmail.com>
2025-06-28 12:00:33 +0530

    added new feature

--- card/src/App.jsx
+++ card/src/App.jsx
@@ -5,9 +5,9 @@ import './App.css'
 const App = () => {
   return (
     <div id='card'>
-      <UserCard name={"Rishi Goyal"} des={"Software Developer"}/>
-      <UserCard name={"Akarsh Sharma"} des={"AI/ML Engineer"}/>
-      <UserCard name = {"Harshit Bhatia"} des={"Technical Support"}/>
+      <UserCard name="Rishi Goyal" des="Software Developer"/>
+      <UserCard name="Akarsh Sharma" des="AI/ML Engineer"/>
+      <UserCard name = "Harshit Bhatia" des="Technical Support"/>
     </div>
   )
 }
